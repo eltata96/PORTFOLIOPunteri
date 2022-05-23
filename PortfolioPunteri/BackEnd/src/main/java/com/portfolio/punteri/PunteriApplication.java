2b3f6f9c1565059d3781d6f257ba96385cb81807
eltata96 <enriquepunteri13@gmail.com>
2022-05-22 21:15:54 -0300

    Inicio de backEnd

---
+++ PortfolioPunteri/BackEnd/src/main/java/com/portfolio/punteri/PunteriApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.punteri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunteriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunteriApplication.class, args);
+	}
+
+}
---
+++ PortfolioPunteri/BackEnd/src/test/java/com/portfolio/punteri/PunteriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.punteri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunteriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
